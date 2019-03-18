4de6c668b44fa2dc6de7e1fe4e40407aa99f2f6d
bkauf@google.com <bkauf@google.com>
2019-03-18 19:18:58 -0400

    changes

--- src/githubssh.sh
+++ src/githubssh.sh
@@ -1,9 +1,4 @@
-eval `ssh-agent`
-#mkdir -p /root/.ssh
-#ssh-keyscan github.com >> ~/.ssh/known_hosts
-mv $1  ~/.ssh/id_rsa
-chmod 600 .ssh/id_rsa
-ssh-add ~/.ssh/id_rsa
-#bundle package --all --all-platforms
+#!/bin/bash
 
-git push git@github.com:bkauf/microservices-demo master
+chmod 600 id_rsa
+ssh-keyscan github.com > /root/.ssh/known_hosts

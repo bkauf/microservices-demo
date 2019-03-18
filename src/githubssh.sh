30436619147595ee3f0a60a60df4e75ec737861d
bkauf@google.com <bkauf@google.com>
2019-03-18 18:46:26 -0400

    changes

---
+++ src/githubssh.sh
@@ -0,0 +1,9 @@
+eval `ssh-agent`
+#mkdir -p /root/.ssh
+#ssh-keyscan github.com >> ~/.ssh/known_hosts
+#mv /workspace/id_rsa  ~/.ssh/id_rsa
+chmod 600 .ssh/id_rsa
+ssh-add ~/.ssh/id_rsa
+#bundle package --all --all-platforms
+
+git push git@github.com:bkauf/microservices-demo master

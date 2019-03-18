e24ae81064265c886cff7fbec9bc6c0dd1be6602
bkauf@google.com <bkauf@google.com>
2019-03-18 15:56:09 -0400

    ssh script for github

--- src/githubssh.sh
+++ src/githubssh.sh
@@ -1,7 +1,9 @@
 eval `ssh-agent`
+apt-get install git -y
 mkdir -p /root/.ssh
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 mv /workspace/id_rsa  ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 ssh-add /workspace/.ssh/id_rsa
 bundle package --all --all-platforms
+git push git@github.com:bkauf/microservices-demo master

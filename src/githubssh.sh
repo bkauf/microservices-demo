051f16598bf4b73b7c3e78c6c14ee0f0e8996b1c
bkauf@google.com <bkauf@google.com>
2019-03-18 18:58:16 -0400

    changes

--- src/githubssh.sh
+++ src/githubssh.sh
@@ -1,7 +1,7 @@
 eval `ssh-agent`
 #mkdir -p /root/.ssh
 #ssh-keyscan github.com >> ~/.ssh/known_hosts
-#mv /workspace/id_rsa  ~/.ssh/id_rsa
+mv $1  ~/.ssh/id_rsa
 chmod 600 .ssh/id_rsa
 ssh-add ~/.ssh/id_rsa
 #bundle package --all --all-platforms
